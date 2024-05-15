837e2328c456f25ecefdeb7f3fada5b672282ea6
Connor J Thompson <ct770a@gmail.com>
2024-05-15 23:42:43 +1000

    updated injection so it accepts the speficied class color

--- js/pagescan.js
+++ js/pagescan.js
@@ -10,11 +10,11 @@ function FindSentenceDelimiter(look_index, direction) {
 function insert(a, position, b){
     return [a.slice(0, position), b, a.slice(position)].join('');
 }
-const match_word_opener = "<b style=\"background-color:#FFFF00; color:#000000\">";
+const match_word_opener = "<b style=\"color:#000000; background-color:"; //\">";
 const match_word_closer = "</b>";
 const match_sentence_opener = "<span style=\"background-color:#b6b0f7; color:#000000\">";
 const match_sentence_closer = "<span>";
-function FindStringSentencesIn(lookfor) {
+function FindStringSentencesIn(lookfor, color) {
     let updated_innerHTML = document.body.innerHTML;
     let match_indicies = [...updated_innerHTML.matchAll(new RegExp(lookfor, 'gi'))].map(a => a.index);
 
@@ -25,23 +25,24 @@ function FindStringSentencesIn(lookfor) {
         // cutout string and push()
         let resulting_sentence = updated_innerHTML.substring(sentence_start, sentence_end).trimStart();
         // close sentence span at the end
-        updated_innerHTML = insert(updated_innerHTML, sentence_end, match_sentence_closer)
+        updated_innerHTML = insert(updated_innerHTML, sentence_end, match_sentence_closer);
         // close span at the end of found word
-        updated_innerHTML = insert(updated_innerHTML, char_index+lookfor.length, match_word_closer)
+        updated_innerHTML = insert(updated_innerHTML, char_index+lookfor.length, match_word_closer);
         // open span at word beginning
-        updated_innerHTML = insert(updated_innerHTML, char_index, match_word_opener)
+        updated_innerHTML = insert(updated_innerHTML, char_index, match_word_opener + color + "\">");
         // open span at sentence beginning
-        updated_innerHTML = insert(updated_innerHTML, sentence_start, match_sentence_opener)
+        updated_innerHTML = insert(updated_innerHTML, sentence_start, match_sentence_opener);
     }
     document.body.innerHTML = updated_innerHTML
 }
+// we need to return [{sentence, matching_word_offset, element_id????}]
 function scanHTML(){
     let matches = []
     for(let filter_index = 0; filter_index < ACTIVE_FILTERS.length; filter_index++){
         let curr_filter = ACTIVE_FILTERS[filter_index];
         for(let filter_entry = 0; filter_entry < curr_filter.entries.length; filter_entry++){
             let curr_entry = curr_filter.entries[filter_entry];
-            FindStringSentencesIn(curr_entry);
+            FindStringSentencesIn(curr_entry, curr_filter.color_code);
         } 
     } 
 }
